f2f15044aae117dc45d539f2f17884d116b5307a
Andrew Matheny <androidmatheny@gmail.com>
2019-10-23 03:33:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,12 @@
 def consolidate_cart(cart)
-  
+  cart = {}
+  cart.each do |item_hash|
+    item_hash.each do |item, attributes|
+      cart[item] ||=attributes
+      cart[item][:count] ? cart[item][:count] +=1 : cart[item][:count] = 1
+    end
+  end
+  cart
 end
 
 def apply_coupons(cart, coupons)
